485b8a2aaee3ff24789fd395e6090f71cb143bc6
EH-llOrJ <jth4115@naver.com>
2022-07-19 13:53:41 +0900

    node js 모든 인스톨 완료

--- 220718/index3.js
+++ 220718/index3.js
@@ -49,6 +49,36 @@ formatting을 해보자
 .vscode폴더를 만들고 안에 settings.json을 만들어 주고
 이곳에 설정한 이유는 개인이 사용하는 vscode 설정 말고 프로젝트 단위로 설정을 적용시킬 수 있다.
 팀이나 회사에서 작업을 하면서 여러명이 작업할 때 설정 값을 미리 정해놓고 작업을 시작하면 병합시 충돌을 덜어준다. 사수한테 이쁨 받을 수 있음.
-*/
 
-let aa;
+참고로 왼쪽 아래 톱니바퀴 설정 혹은 컨트롤 + , 에서 오른쪽 위 아이콘 설정 열기(json)으로 들어가면 settings.json 나옴
+
+Linting을 해보자
+Linting에서 좋은 거 ESlint 패키지이자 플러그인
+ESLint 설치 명령어
+--save-dev 개발환경이니까
+
+--------------------------------
+npm install --save-dev eslint
+--------------------------------
+
+lock.json에 무적 많이 생기는데 의존성들(서브 디펜던시)
+의존성의 뜻은 코드에서 두 모듈 간의 연결이라고 보면 된다.
+클래스가 두개 있다 치면 두 클래스의 관계성
+그냥 쓸려고 패키지 다운 받는다고 보면 된다.
+
+eslint도 설정 파일이 필요한데
+이 설정 파일은 확장자가 필요하다. js
+설정파일 이름은 .eslintrc.js 이렇게 작성
+
+//마켓 플레이스에서 설치해야 할 것 ESLint 설치
+
+//rc 뜻은 
+runtime configuration
+run control
+run commands
+runcom
+resoure control
+
+
+
+*/
